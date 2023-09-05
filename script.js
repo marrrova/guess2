580d2d44466601932f5c93b839523b55e242389c
marrrova <marisloth.kh@gmail.com>
2023-09-05 20:21:20 +0200

    написали підказки JS

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-let sound = new Audio;
+let sound = new Audio();
 sound.src = "sound.mp3"
 sound.volume = 0.05
 sound.play()
 
-let n =  rand(); let s = 1; let flag = false; function get() {return document.getElementById('r')}
+let n = rand();
+//створюємо випадкове число
+let s = 1;
+//ми фіксуємо скільки разів гравець натискав
+let flag = false;
+//флажок для фіксації виграшу чи поразки
+function get() {
+return document.getElementById('r')
+}
+//взяли текст відповіді
 function guess() {
-  let a = document.getElementById('v').value; d = parseInt(a, 10);
+//основна функція гри
+  let a = document.getElementById('v').value;
+  //записую відповідь гравця 
+  d = parseInt(a, 10);
+//збереження відповідей як числа (10-десяткова система числення можна просто написати Number(a))
   if (s <= 3) {
-    if (d === n) {get().innerText = "Ура! Ви перемогли!"; s = 4; flag = true;
-    p.style.visibility = "visible"
-    p.src = "img/win.gif";
-  }
-    else if (d < n) {get().innerText = "Число більше";
-    document.getElementById('v').innerText='';
+    //якщо кількість відповідей менша рівна трьом
+    if (d === n) {
+      get().innerText = "Ура! Ви перемогли!";
+      //якщо співпадає
+      s = 4;
+      //вихід
+      flag = true;
+      //флажок піднято
+      p.style.visibility = "visible"
+      //показуєм картинку
+      p.src = "img/win.gif";
+      //підтягуємо картинку
     }
-    else if (d > n) {get().innerText = "Число менше";
-    document.getElementById('v').innerText='';
+    else if (d < n){
+      get().innerText = "Число більше";
+      //підказка
+      document.getElementById('v').innerText = '';
+      //відповідь витераеться
     }
-    s++; if (s > 3 && flag !== true) {get().innerText = n + ", Ви програли!"
-    p.style.visibility = "visible"
-}
+    else if (d > n) {
+      get().innerText = "Число менше";
+      //підказка
+      document.getElementById('v').innerText = '';
+      //відповідь витераеться
+    }
+    s++;
+    //нарощуємо 
+    if (s > 3 && flag !== true) {
+      get().innerText = n + ", Ви програли!"
+      p.style.visibility = "visible"
+      //програш
+    }
   }
 }
-function rand(){return Math.floor(Math.random()*10)}
-function game(){window.location.reload()}
+function rand() {
+  return Math.floor(Math.random() * 10)
+  //викідання випадкового числа
+}
+
+function game() {
+  window.location.reload()
+  //перезавантаження сторінки
+}
+
 
 let p = document.getElementById("picture");
-console.log(p);
+
 p.style.visibility = "hidden";
+//захоплюєм картинку і робимо її не видимою
